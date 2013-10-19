fb7cb14cd898bbe5be450145e7838700be47976a
Numerico <webmaster@numerica.cl>
2013-10-19 19:25:25 -0300

    rails 4 app without test unit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dturno::Application.config.secret_key_base = '74c13b3867d00e13aa60ca92cdb4000167ba43494a7b524be86535b9ecca94f81de640c9073f0ec65756d8a7b1ee2e669d3eded0d327eca586d022c27c03dd5b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Dturno::Application.config.session_store :cookie_store, key: '_dturno_session'
